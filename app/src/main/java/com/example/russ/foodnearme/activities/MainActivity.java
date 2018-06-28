c711d084f9a3359427982610954aac0673b2099c
russ226 <russ2510@gmail.com>
2018-06-28 00:07:56 -0400

    adding settings activity and styling main activity

--- app/src/main/java/com/example/russ/foodnearme/activities/MainActivity.java
+++ app/src/main/java/com/example/russ/foodnearme/activities/MainActivity.java
@@ -12,6 +12,9 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.Manifest;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,19 +29,40 @@ public class MainActivity extends AppCompatActivity {
     public static final int REQUEST_CODE_ASK_PERMISSIONS = 1;
     private static final String[] REQUIRED_SDK_PERMISSIONS = new String[] {
             Manifest.permission.ACCESS_FINE_LOCATION};
-    LocationManager locationManager;
-    String provider;
+
+    private TextView selectCuisine, settings;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        selectCuisine = (TextView)findViewById(R.id.go_to_cuisine);
+        settings = (TextView)findViewById(R.id.settings);
 
-        
+        selectCuisine.setOnTouchListener(new OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Intent i = new Intent(getApplicationContext(), FoodChoice.class);
+                startActivity(i);
+
+                return false;
+            }
+        });
+
+        settings.setOnTouchListener(new OnTouchListener(){
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                Intent i = new Intent(getApplicationContext(), FoodChoice.class);
+                startActivity(i);
+
+                return false;
+            }
+        });
+
         checkPermissions();
-        //Intent i = new Intent(MainActivity.this, FoodChoice.class);
-        //startActivity(i);
+
     }
 
     protected void checkPermissions() {
